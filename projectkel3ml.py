49e8f6aa6ba5632c9e5aebea73e40f535dea4d38
Naokturnal <72050624+nao0856@users.noreply.github.com>
2025-06-18 10:01:48 +0700

    Update projectkel3ml.py

--- projectkel3ml.py
+++ projectkel3ml.py
@@ -11,6 +11,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
